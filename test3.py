8633626d5748dba06e84707cb74d77fb57782d5e
TovM24 <tovm24xinchao@gmail.com>
2023-09-23 21:18:53 +0700

    Commit đầu tiên trên nhánh main

---
+++ test3.py
@@ -0,0 +1,20 @@
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def detect_browser():
+#     user_agent = request.headers.get('User-Agent')
+    
+#     if 'Cốc Cốc' in user_agent:
+#         return "Bạn đang sử dụng trình duyệt Cốc Cốc."
+#     elif 'Chrome' in user_agent:
+#         return "Bạn đang sử dụng trình duyệt Chrome."
+#     else:
+#         return "Không thể xác định trình duyệt."
+
+# if __name__ == '__main__':
+#     app.run()
+
+
+print("Xin chao");
